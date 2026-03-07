c703397c052061441c04ce13de024ebf5815177f
Saad Raza <razasaad118@gmail.com>
2026-03-08 02:57:47 +0500

    first commit

---
+++ src/apps/index.js
@@ -0,0 +1,85 @@
+import Home from './home';
+import Contact from './contact';
+import barba from '@barba/core';
+import GSAP from 'gsap';
+import _ from '../scss/main.scss';
+
+
+
+
+class App{
+    constructor(){
+
+
+        this.pages = {
+            home : new Home(),
+            contact : new Contact(),
+            
+        }
+        
+
+        this.createAjaxNavigation()
+        this.createReRender()
+        this.addEventListeners()
+    }
+    createAjaxNavigation(){
+
+        const easeIn = (container,done)=> {
+            return GSAP.to(container, {
+                autoAlpha: 0,
+                duration: 1,
+                ease: 'none',
+                onComplete: ()=> done()
+            })
+        }
+
+        const  easeOut = (container) => {
+
+            return GSAP.from(container, {
+                autoAlpha: 0,
+                duration: 1,
+                ease: 'none',
+            })
+        }
+
+       barba.init({
+                preventRunning: true,
+                transitions: [
+                {
+                once({ next }) {
+                     easeOut(next.container);
+                },
+                leave({ current }) {
+                    const done = this.async();
+                    easeIn(current.container, done);
+                },
+                enter({ next }) {
+                     easeOut(next.container);
+                }
+                }
+            ],
+            
+        })
+        
+    }
+    
+    createReRender(){
+        
+        barba.hooks.before(() => {
+        })
+    
+        barba.hooks.after(() => {
+            this.pages.home.createReRender() 
+            this.pages.contact.createReRender() 
+        })
+    }
+   
+    
+    addEventListeners(){
+        
+        
+        
+    }
+}
+
+new App()
---
+++ tailwind.config.js
@@ -0,0 +1,37 @@
+/** @type {import('tailwindcss').Config} */
+/**
+ * fluid function
+ */
+import { Fluid } from './src/apps/extra/math.js'
+const daisyui = require('daisyui')
+
+module.exports = {
+  content: ["./src/**/*.{html,js,pug}"],
+  theme: {
+    container: {
+      center: true,
+      screens: {
+        DEFAULT: '100%', 
+        xl: '1440px',    
+      },
+    },
+    extend: {
+      fontSize: {        
+        xxl: [Fluid(4, 200)], 
+        xl: [Fluid(4, 120)], 
+        lg: [], 
+        md: [[Fluid(1.2, 3)]], 
+        sm: [], 
+        body: [], 
+        input: [], 
+        highlight: [Fluid(1.2, 1.2)], 
+      },      
+    },
+  },
+  plugins: [
+    daisyui
+  ],
+  corePlugins: {
+    preflight: true, /** Ensure it's enabled */
+  },
+}
