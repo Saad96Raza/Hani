e4672cb4a77d92d7e254a4da1285e4fef7f00724
Saad Raza <razasaad118@gmail.com>
2026-03-14 11:43:40 +0500

    Updates

--- js/main.bundle.js
+++ js/main.bundle.js
@@ -34,551 +34,6 @@ __webpack_require__.r(__webpack_exports__);
 
 
 },
-"./node_modules/@splidejs/splide-extension-auto-scroll/dist/js/splide-extension-auto-scroll.esm.js"(__unused_rspack_module, __webpack_exports__, __webpack_require__) {
-"use strict";
-__webpack_require__.r(__webpack_exports__);
-__webpack_require__.d(__webpack_exports__, {
-  AutoScroll: () => (AutoScroll)
-});
-/*!
- * @splidejs/splide-extension-auto-scroll
- * Version  : 0.5.3
- * License  : MIT
- * Copyright: 2022 Naotoshi Fujita
- */
-function empty(array) {
-  array.length = 0;
-}
-
-function slice$1(arrayLike, start, end) {
-  return Array.prototype.slice.call(arrayLike, start, end);
-}
-
-function apply$1(func) {
-  return func.bind.apply(func, [null].concat(slice$1(arguments, 1)));
-}
-
-function raf(func) {
-  return requestAnimationFrame(func);
-}
-
-function typeOf$1(type, subject) {
-  return typeof subject === type;
-}
-
-var isArray$1 = Array.isArray;
-apply$1(typeOf$1, "function");
-apply$1(typeOf$1, "string");
-apply$1(typeOf$1, "undefined");
-
-function toArray$1(value) {
-  return isArray$1(value) ? value : [value];
-}
-
-function forEach$1(values, iteratee) {
-  toArray$1(values).forEach(iteratee);
-}
-
-var ownKeys$1 = Object.keys;
-
-function forOwn$1(object, iteratee, right) {
-  if (object) {
-    var keys = ownKeys$1(object);
-    keys = right ? keys.reverse() : keys;
-
-    for (var i = 0; i < keys.length; i++) {
-      var key = keys[i];
-
-      if (key !== "__proto__") {
-        if (iteratee(object[key], key) === false) {
-          break;
-        }
-      }
-    }
-  }
-
-  return object;
-}
-
-function assign$1(object) {
-  slice$1(arguments, 1).forEach(function (source) {
-    forOwn$1(source, function (value, key) {
-      object[key] = source[key];
-    });
-  });
-  return object;
-}
-
-var min$1 = Math.min;
-
-function EventBinder() {
-  var listeners = [];
-
-  function bind(targets, events, callback, options) {
-    forEachEvent(targets, events, function (target, event, namespace) {
-      var isEventTarget = ("addEventListener" in target);
-      var remover = isEventTarget ? target.removeEventListener.bind(target, event, callback, options) : target["removeListener"].bind(target, callback);
-      isEventTarget ? target.addEventListener(event, callback, options) : target["addListener"](callback);
-      listeners.push([target, event, namespace, callback, remover]);
-    });
-  }
-
-  function unbind(targets, events, callback) {
-    forEachEvent(targets, events, function (target, event, namespace) {
-      listeners = listeners.filter(function (listener) {
-        if (listener[0] === target && listener[1] === event && listener[2] === namespace && (!callback || listener[3] === callback)) {
-          listener[4]();
-          return false;
-        }
-
-        return true;
-      });
-    });
-  }
-
-  function dispatch(target, type, detail) {
-    var e;
-    var bubbles = true;
-
-    if (typeof CustomEvent === "function") {
-      e = new CustomEvent(type, {
-        bubbles: bubbles,
-        detail: detail
-      });
-    } else {
-      e = document.createEvent("CustomEvent");
-      e.initCustomEvent(type, bubbles, false, detail);
-    }
-
-    target.dispatchEvent(e);
-    return e;
-  }
-
-  function forEachEvent(targets, events, iteratee) {
-    forEach$1(targets, function (target) {
-      target && forEach$1(events, function (events2) {
-        events2.split(" ").forEach(function (eventNS) {
-          var fragment = eventNS.split(".");
-          iteratee(target, fragment[0], fragment[1]);
-        });
-      });
-    });
-  }
-
-  function destroy() {
-    listeners.forEach(function (data) {
-      data[4]();
-    });
-    empty(listeners);
-  }
-
-  return {
-    bind: bind,
-    unbind: unbind,
-    dispatch: dispatch,
-    destroy: destroy
-  };
-}
-var EVENT_MOVE = "move";
-var EVENT_MOVED = "moved";
-var EVENT_UPDATED = "updated";
-var EVENT_DRAG = "drag";
-var EVENT_DRAGGED = "dragged";
-var EVENT_SCROLL = "scroll";
-var EVENT_SCROLLED = "scrolled";
-var EVENT_DESTROY = "destroy";
-
-function EventInterface(Splide2) {
-  var bus = Splide2 ? Splide2.event.bus : document.createDocumentFragment();
-  var binder = EventBinder();
-
-  function on(events, callback) {
-    binder.bind(bus, toArray$1(events).join(" "), function (e) {
-      callback.apply(callback, isArray$1(e.detail) ? e.detail : []);
-    });
-  }
-
-  function emit(event) {
-    binder.dispatch(bus, event, slice$1(arguments, 1));
-  }
-
-  if (Splide2) {
-    Splide2.event.on(EVENT_DESTROY, binder.destroy);
-  }
-
-  return assign$1(binder, {
-    bus: bus,
-    on: on,
-    off: apply$1(binder.unbind, bus),
-    emit: emit
-  });
-}
-
-function RequestInterval(interval, onInterval, onUpdate, limit) {
-  var now = Date.now;
-  var startTime;
-  var rate = 0;
-  var id;
-  var paused = true;
-  var count = 0;
-
-  function update() {
-    if (!paused) {
-      rate = interval ? min$1((now() - startTime) / interval, 1) : 1;
-      onUpdate && onUpdate(rate);
-
-      if (rate >= 1) {
-        onInterval();
-        startTime = now();
-
-        if (limit && ++count >= limit) {
-          return pause();
-        }
-      }
-
-      raf(update);
-    }
-  }
-
-  function start(resume) {
-    !resume && cancel();
-    startTime = now() - (resume ? rate * interval : 0);
-    paused = false;
-    raf(update);
-  }
-
-  function pause() {
-    paused = true;
-  }
-
-  function rewind() {
-    startTime = now();
-    rate = 0;
-
-    if (onUpdate) {
-      onUpdate(rate);
-    }
-  }
-
-  function cancel() {
-    id && cancelAnimationFrame(id);
-    rate = 0;
-    id = 0;
-    paused = true;
-  }
-
-  function set(time) {
-    interval = time;
-  }
-
-  function isPaused() {
-    return paused;
-  }
-
-  return {
-    start: start,
-    rewind: rewind,
-    pause: pause,
-    cancel: cancel,
-    set: set,
-    isPaused: isPaused
-  };
-}
-
-function Throttle(func, duration) {
-  var interval;
-
-  function throttled() {
-    if (!interval) {
-      interval = RequestInterval(duration || 0, function () {
-        func();
-        interval = null;
-      }, null, 1);
-      interval.start();
-    }
-  }
-
-  return throttled;
-}
-var CLASS_ACTIVE = "is-active";
-
-var SLIDE = "slide";
-var FADE = "fade";
-
-function slice(arrayLike, start, end) {
-  return Array.prototype.slice.call(arrayLike, start, end);
-}
-
-function apply(func) {
-  return func.bind(null, ...slice(arguments, 1));
-}
-
-function typeOf(type, subject) {
-  return typeof subject === type;
-}
-function isObject(subject) {
-  return !isNull(subject) && typeOf("object", subject);
-}
-const isArray = Array.isArray;
-apply(typeOf, "function");
-apply(typeOf, "string");
-const isUndefined = apply(typeOf, "undefined");
-function isNull(subject) {
-  return subject === null;
-}
-
-function toArray(value) {
-  return isArray(value) ? value : [value];
-}
-
-function forEach(values, iteratee) {
-  toArray(values).forEach(iteratee);
-}
-
-function toggleClass(elm, classes, add) {
-  if (elm) {
-    forEach(classes, (name) => {
-      if (name) {
-        elm.classList[add ? "add" : "remove"](name);
-      }
-    });
-  }
-}
-
-const ownKeys = Object.keys;
-
-function forOwn(object, iteratee, right) {
-  if (object) {
-    let keys = ownKeys(object);
-    keys = right ? keys.reverse() : keys;
-    for (let i = 0; i < keys.length; i++) {
-      const key = keys[i];
-      if (key !== "__proto__") {
-        if (iteratee(object[key], key) === false) {
-          break;
-        }
-      }
-    }
-  }
-  return object;
-}
-
-function assign(object) {
-  slice(arguments, 1).forEach((source) => {
-    forOwn(source, (value, key) => {
-      object[key] = source[key];
-    });
-  });
-  return object;
-}
-
-function removeAttribute(elms, attrs) {
-  forEach(elms, (elm) => {
-    forEach(attrs, (attr) => {
-      elm && elm.removeAttribute(attr);
-    });
-  });
-}
-
-function setAttribute(elms, attrs, value) {
-  if (isObject(attrs)) {
-    forOwn(attrs, (value2, name) => {
-      setAttribute(elms, name, value2);
-    });
-  } else {
-    forEach(elms, (elm) => {
-      isNull(value) || value === "" ? removeAttribute(elm, attrs) : elm.setAttribute(attrs, String(value));
-    });
-  }
-}
-
-const { min, max, floor, ceil, abs } = Math;
-
-function clamp(number, x, y) {
-  const minimum = min(x, y);
-  const maximum = max(x, y);
-  return min(max(minimum, number), maximum);
-}
-
-const DEFAULTS = {
-  speed: 1,
-  autoStart: true,
-  pauseOnHover: true,
-  pauseOnFocus: true
-};
-
-const I18N = {
-  startScroll: "Start auto scroll",
-  pauseScroll: "Pause auto scroll"
-};
-
-function AutoScroll(Splide2, Components2, options) {
-  const { on, off, bind, unbind } = EventInterface(Splide2);
-  const { translate, getPosition, toIndex, getLimit } = Components2.Move;
-  const { setIndex, getIndex } = Components2.Controller;
-  const { orient } = Components2.Direction;
-  const { toggle } = Components2.Elements;
-  const { Live } = Components2;
-  const { root } = Splide2;
-  const throttledUpdateArrows = Throttle(Components2.Arrows.update, 500);
-  let autoScrollOptions = {};
-  let interval;
-  let stopped;
-  let hovered;
-  let focused;
-  let busy;
-  let currPosition;
-  function setup() {
-    const { autoScroll } = options;
-    autoScrollOptions = assign({}, DEFAULTS, isObject(autoScroll) ? autoScroll : {});
-  }
-  function mount() {
-    if (!Splide2.is(FADE)) {
-      if (!interval && options.autoScroll !== false) {
-        interval = RequestInterval(0, move);
-        listen();
-        autoStart();
-      }
-    }
-  }
-  function destroy() {
-    if (interval) {
-      interval.cancel();
-      interval = null;
-      currPosition = void 0;
-      off([EVENT_MOVE, EVENT_DRAG, EVENT_SCROLL, EVENT_MOVED, EVENT_SCROLLED]);
-      unbind(root, "mouseenter mouseleave focusin focusout");
-      unbind(toggle, "click");
-    }
-  }
-  function listen() {
-    if (autoScrollOptions.pauseOnHover) {
-      bind(root, "mouseenter mouseleave", (e) => {
-        hovered = e.type === "mouseenter";
-        autoToggle();
-      });
-    }
-    if (autoScrollOptions.pauseOnFocus) {
-      bind(root, "focusin focusout", (e) => {
-        focused = e.type === "focusin";
-        autoToggle();
-      });
-    }
-    if (autoScrollOptions.useToggleButton) {
-      bind(toggle, "click", () => {
-        stopped ? play() : pause();
-      });
-    }
-    on(EVENT_UPDATED, update);
-    on([EVENT_MOVE, EVENT_DRAG, EVENT_SCROLL], () => {
-      busy = true;
-      pause(false);
-    });
-    on([EVENT_MOVED, EVENT_DRAGGED, EVENT_SCROLLED], () => {
-      busy = false;
-      autoToggle();
-    });
-  }
-  function update() {
-    const { autoScroll } = options;
-    if (autoScroll !== false) {
-      autoScrollOptions = assign({}, autoScrollOptions, isObject(autoScroll) ? autoScroll : {});
-      mount();
-    } else {
-      destroy();
-    }
-    if (interval && !isUndefined(currPosition)) {
-      translate(currPosition);
-    }
-  }
-  function autoStart() {
-    if (autoScrollOptions.autoStart) {
-      if (document.readyState === "complete") {
-        play();
-      } else {
-        bind(window, "load", play);
-      }
-    }
-  }
-  function play() {
-    if (isPaused()) {
-      interval.start(true);
-      Live.disable(true);
-      focused = hovered = stopped = false;
-      updateButton();
-    }
-  }
-  function pause(stop = true) {
-    if (!stopped) {
-      stopped = stop;
-      updateButton();
-      if (!isPaused()) {
-        interval.pause();
-        Live.disable(false);
-      }
-    }
-  }
-  function autoToggle() {
-    if (!stopped) {
-      hovered || focused || busy ? pause(false) : play();
-    }
-  }
-  function move() {
-    const position = getPosition();
-    const destination = computeDestination(position);
-    if (position !== destination) {
-      translate(destination);
-      updateIndex(currPosition = getPosition());
-    } else {
-      pause(false);
-      if (autoScrollOptions.rewind) {
-        Splide2.go(autoScrollOptions.speed > 0 ? 0 : Components2.Controller.getEnd());
-      }
-    }
-    throttledUpdateArrows();
-  }
-  function computeDestination(position) {
-    const speed = autoScrollOptions.speed || 1;
-    position += orient(speed);
-    if (Splide2.is(SLIDE)) {
-      position = clamp(position, getLimit(false), getLimit(true));
-    }
-    return position;
-  }
-  function updateIndex(position) {
-    const { length } = Splide2;
-    const index = (toIndex(position) + length) % length;
-    if (index !== getIndex()) {
-      setIndex(index);
-      Components2.Slides.update();
-      Components2.Pagination.update();
-      options.lazyLoad === "nearby" && Components2.LazyLoad.check();
-    }
-  }
-  function updateButton() {
-    if (toggle) {
-      const key = stopped ? "startScroll" : "pauseScroll";
-      toggleClass(toggle, CLASS_ACTIVE, !stopped);
-      setAttribute(toggle, "aria-label", options.i18n[key] || I18N[key]);
-    }
-  }
-  function isPaused() {
-    return !interval || interval.isPaused();
-  }
-  return {
-    setup,
-    mount,
-    destroy,
-    play,
-    pause,
-    isPaused
-  };
-}
-
-
-
-
-},
 "./node_modules/@splidejs/splide/dist/js/splide.esm.js"(__unused_rspack_module, __webpack_exports__, __webpack_require__) {
 "use strict";
 __webpack_require__.r(__webpack_exports__);
@@ -28825,8 +28280,7 @@ __webpack_require__.d(__webpack_exports__, {
   "default": () => (Home)
 });
 /* import */ var _splidejs_splide__rspack_import_0 = __webpack_require__("./node_modules/@splidejs/splide/dist/js/splide.esm.js");
-/* import */ var _splidejs_splide_extension_auto_scroll__rspack_import_1 = __webpack_require__("./node_modules/@splidejs/splide-extension-auto-scroll/dist/js/splide-extension-auto-scroll.esm.js");
-/* import */ var _splidejs_splide_css_core__rspack_import_2 = __webpack_require__("./node_modules/@splidejs/splide/dist/css/splide-core.min.css");
+/* import */ var _splidejs_splide_css_core__rspack_import_1 = __webpack_require__("./node_modules/@splidejs/splide/dist/css/splide-core.min.css");
 function _class_call_check(instance, Constructor) {
     if (!(instance instanceof Constructor)) {
         throw new TypeError("Cannot call a class as a function");
@@ -28848,7 +28302,6 @@ function _create_class(Constructor, protoProps, staticProps) {
 }
 
 
-
 var Home = /*#__PURE__*/ function() {
     "use strict";
     function Home() {
@@ -28865,20 +28318,19 @@ var Home = /*#__PURE__*/ function() {
         {
             key: "createModelsSlider",
             value: function createModelsSlider() {
-                this.splideHome = document.querySelector(".home-gallery-slider");
-                if (this.splideHome) {
-                    this.splide = new _splidejs_splide__rspack_import_0["default"](this.splideHome, {
+                var _this = this;
+                this.splideWrapper = document.querySelector(".home-gallery-slider");
+                if (this.splideWrapper) {
+                    this.splide = new _splidejs_splide__rspack_import_0["default"](this.splideWrapper, {
                         type: 'loop',
                         drag: 'free',
-                        perPage: 5,
-                        autoScroll: {
-                            speed: 1,
-                            pauseOnHover: false,
-                            pauseOnFocus: false,
-                            rewind: false
-                        },
+                        interval: 3000,
+                        autoplay: true,
+                        speed: 2000,
+                        pauseOnHover: false,
+                        perPage: 4,
                         arrows: false,
-                        focus: 'center',
+                        focus: 'start',
                         gap: '1rem',
                         breakpoints: {
                             1280: {
@@ -28895,15 +28347,13 @@ var Home = /*#__PURE__*/ function() {
                             }
                         }
                     });
-                    // this.bar = this.splide.root.querySelector('.my-slider-progress-bar')
-                    // this.splide.on('mounted move', () => {
-                    //     const end  = this.splide.Components.Controller.getEnd() + 1
-                    //     const rate = Math.min((this.splide.index + 1) / end, 1)
-                    //     this.bar.style.width = (100 * rate) + '%'
-                    // })
-                    this.splide.mount({
-                        AutoScroll: _splidejs_splide_extension_auto_scroll__rspack_import_1.AutoScroll
+                    this.bar = this.splide.root.querySelector('.my-slider-progress-bar');
+                    this.splide.on('mounted move', function() {
+                        var end = _this.splide.Components.Controller.getEnd() + 1;
+                        var rate = Math.min((_this.splide.index + 1) / end, 1);
+                        _this.bar.style.width = 100 * rate + '%';
                     });
+                    this.splide.mount();
                 }
             }
         }
@@ -28978,10 +28428,10 @@ var SingleModel = /*#__PURE__*/ function() {
         {
             key: "createSlider",
             value: function createSlider() {
-                this.singleModel = document.querySelector(".single-model-slider");
-                if (this.singleModel) {
+                this.splideWrapper = document.querySelector(".single-model-slider");
+                if (this.splideWrapper) {
                     var _obj;
-                    new _splidejs_splide__rspack_import_1["default"](this.singleModel, (_obj = {
+                    new _splidejs_splide__rspack_import_1["default"](this.splideWrapper, (_obj = {
                         type: 'loop',
                         drag: 'free',
                         perPage: 3,
@@ -29175,6 +28625,7 @@ var App = /*#__PURE__*/ function() {
         };
         this.offCanvas = document.querySelector('#offcanvas');
         this.links = document.querySelectorAll('.menu li a');
+        this.buttons = document.querySelectorAll('.button-1-stroke');
         this.createAjaxNavigation();
         this.createReRender();
         this.addEventListeners();
@@ -29242,7 +28693,16 @@ var App = /*#__PURE__*/ function() {
         },
         {
             key: "addEventListeners",
-            value: function addEventListeners() {}
+            value: function addEventListeners() {
+                lodash__rspack_import_5_default().forEach(this.buttons, function(element) {
+                    element.addEventListener('mouseenter', function() {
+                        if (!element.classList.contains('is-animating')) element.classList.add('is-animating');
+                        element.addEventListener('animationend', function() {
+                            return element.classList.remove('is-animating');
+                        });
+                    });
+                });
+            }
         }
     ]);
     return App;
